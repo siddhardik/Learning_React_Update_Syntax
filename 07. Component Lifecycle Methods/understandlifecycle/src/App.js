9363742323b63e6bdf6a994db939f51f11fcfb99
siddhardik <siddharthabhunia2001@gmail.com>
2023-08-13 12:50:05 +0530

    Component Life Cycle

---
+++ 07. Component Lifecycle Methods/understandlifecycle/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ComponentA from './ComponentA';
+
+class App extends React.Component {
+  
+render(){
+    return  (
+      // <h1>Hello</h1>
+      <ComponentA />
+    )
+}
+
+}
+
+export default App;
