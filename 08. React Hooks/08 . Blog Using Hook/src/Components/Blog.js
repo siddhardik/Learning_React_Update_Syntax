97f846ed0ad9168a579b9a5cf5f9c699e094af99
siddhardik <siddharthabhunia2001@gmail.com>
2023-08-19 20:20:34 +0530

    useReducer

--- 08. React Hooks/08 . Blog Using Hook/src/Components/Blog.js
+++ 08. React Hooks/08 . Blog Using Hook/src/Components/Blog.js
@@ -1,13 +1,27 @@
 //Blogging App using Hooks
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useReducer } from "react";
 
-export default function Blog(){
+function blogsReducer(state, action) {
+    switch (action.type) {
+        case "ADD":
+            return [action.blog, ...state]
+
+        case "REMOVE":
+            return (state.filter((blog, index) => index !== action.index));
+
+        default:
+            return [];    
+    }
+}
+
+export default function Blog() {
 
     // const [title,setTitle] = useState("");
     // const [content,setContent] = useState("");
-    const [formData, setformData] = useState({title:"", content:""})
-    const [blogs, setBlogs] =  useState([]);
-    
+    const [formData, setformData] = useState({ title: "", content: "" })
+    // const [blogs, setBlogs] =  useState([]);
+    const [blogs, dispatch] = useReducer(blogsReducer, []);
+
     //useRef hook initialized
     const titleRef = useRef(null);
 
@@ -21,102 +35,104 @@ export default function Blog(){
     // Focus in Title input on mount
     useEffect(() => {
         titleRef.current.focus();
-    },[]);
+    }, []);
 
     useEffect(() => {
         // 3. Required to add Title of the latest blog as page's title
         // Show Dependency Injection of blogs
         // Helps us avoid rerun logic on title and content change
         // Still has both DidMount and DidUpdate feature
-        
+
         console.log("Runs on Blogs Mount/Update!!");
         if (blogs.length && blogs[0].title) {
-          document.title = blogs[0].title;
+            document.title = blogs[0].title;
         } else {
-          document.title = "No blogs!";
+            document.title = "No blogs!";
         }
-      }, [blogs]);
+    }, [blogs]);
 
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault();
 
-        setBlogs([{title: formData.title,content:formData.content}, ...blogs]);
-        setformData({title:"", content:""});
+        // setBlogs([{ title: formData.title, content: formData.content }, ...blogs]);
+        dispatch({ type: "ADD", blog: { title: formData.title, content: formData.content } });
+        setformData({ title: "", content: "" });
         //Setting focus on title after adding a blog
         titleRef.current.focus();
         console.log(blogs);
     }
 
-    function removeBlog(i){
+    function removeBlog(i) {
 
-        setBlogs( blogs.filter((blog,index)=> index !== i));
- 
-     }
+        // setBlogs(blogs.filter((blog, index) => index !== i));
+        dispatch({ type: "REMOVE", index: i })
 
-    return(
+    }
+
+    return (
         <>
-        <h1>Write a Blog!</h1>
-        <div className="section">
+            <h1>Write a Blog!</h1>
+            <div className="section">
 
-        {/* Form for to write the blog */}
-            <form onSubmit={handleSubmit}>
-                <Row label="Title">
+                {/* Form for to write the blog */}
+                <form onSubmit={handleSubmit}>
+                    <Row label="Title">
                         <input className="input"
-                                placeholder="Enter the Title of the Blog here.."
-                                value={formData.title}
-                                ref = {titleRef}
-                                onChange = {(e) => setformData({title: e.target.value, content:formData.content})}
+                            placeholder="Enter the Title of the Blog here.."
+                            value={formData.title}
+                            ref={titleRef}
+                            onChange={(e) => setformData({ title: e.target.value, content: formData.content })}
                         />
-                </Row >
+                    </Row >
 
-                <Row label="Content">
+                    <Row label="Content">
                         <textarea className="input content"
-                                placeholder="Content of the Blog goes here.."
-                                value={formData.content}
-                                onChange = {(e) => setformData({title: formData.title,content: e.target.value})}
+                            placeholder="Content of the Blog goes here.."
+                            value={formData.content}
+                            onChange={(e) => setformData({ title: formData.title, content: e.target.value })}
                         />
-                </Row >
-         
-                <button className = "btn">ADD</button>
-            </form>
-                     
-        </div>
+                    </Row >
 
-        <hr/>
+                    <button className="btn">ADD</button>
+                </form>
 
-        {/* Section where submitted blogs will be displayed */}
-        <h2> Blogs </h2>
-        {blogs.map((blog,i) => (
-            <div className="blog">
-                <h3>{blog.title}</h3>
-                <hr/>
-                <p>{blog.content}</p>
+            </div>
 
-                <div className="blog-btn">
+            <hr />
+
+            {/* Section where submitted blogs will be displayed */}
+            <h2> Blogs </h2>
+            {blogs.map((blog, i) => (
+                <div className="blog">
+                    <h3>{blog.title}</h3>
+                    <hr />
+                    <p>{blog.content}</p>
+
+                    <div className="blog-btn">
                         <button onClick={() => {
                             removeBlog(i)
                         }}
-                        className="btn remove">
+                            className="btn remove">
 
                             Delete
 
                         </button>
+                    </div>
                 </div>
-            </div>
-        ))}
-        
+            ))}
+
         </>
-        )
-    }
+    )
+}
 
 //Row component to introduce a new row section in the form
-function Row(props){
-    const {label} = props;
-    return(
+function Row(props) {
+    const { label } = props;
+    return (
         <>
-        <label >{label}<br/></label>
-        {props.children}
-        <hr />
+            <label >{label}<br /></label>
+            {props.children}
+            <hr />
         </>
     )
 }
