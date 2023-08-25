e00317f87870774f0c840848198ef4783112bd15
siddhardik <siddharthabhunia2001@gmail.com>
2023-08-25 14:02:47 +0530

    Navigate With ErrorPage

--- 09. React Router/Intro-to-Routing-unhandled-routes/src/pages/ErrorPage.js
+++ 09. React Router/Intro-to-Routing-unhandled-routes/src/pages/ErrorPage.js
@@ -3,9 +3,10 @@ import { useNavigate } from "react-router-dom";
 function ErrorPage() {
   const navigate = useNavigate();
   useEffect(() => {
-    setTimeout(() => navigate(-1), 3000);
+    setTimeout(() => navigate("/about"), 3000);
   }, []);
-
+  // -1 means 1 step back from where It was requested   to back home page 
+// Specify any route 
   return (
     <>
       <h1>OOOPPSSS!!!!! Something went wrong</h1>
